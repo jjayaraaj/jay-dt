d030ad29486d0de0eef73cdf47d7777bb9b04fb9
jjayaraaj <jjayaraaj@gmail.com>
2024-09-02 09:54:53 -0400

    Hide instagram from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -357,11 +357,11 @@ export default async function Home() {
           </p>
           <div className="mt-6 flex gap-6">
             {/* <SocialLink href="#" aria-label="Follow on X" icon={XIcon} /> */}
-            <SocialLink
+{/*             <SocialLink
               href="https://www.instagram.com/its_me_jayho/"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
-            />
+            /> */}
             <SocialLink
               href="https://github.com/jjayaraaj"
               aria-label="Follow on GitHub"
